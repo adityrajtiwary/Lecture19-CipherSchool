cf77ca71b62f54116c34d230ac0bbe3912d0c694
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-08-25 09:30:25 +0530

    Add files via upload

---
+++ Lecture19-CipherSchool.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long int a;
+	int b;
+	cout<<"The size of int is:"<<sizeof(int);
+	cout<<"The size of long long int: "<<sizeof(a);
+	cout<<"The size of b: "<<sizeof(b);
+	return 0;
+}
